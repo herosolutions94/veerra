8d70af659e6eab2975b99c2025996c98ad32fea6
herosolutions94 <herosolutions.tk@gmail.com>
2024-07-05 18:19:39 +0500

    home

---
+++ components/banner-tabs.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useRef } from "react";
+import Link from 'next/link'
+import Image from 'next/image';
+
+export default function BannerTabs() {
+   
+    const [tab, setTab] = useState(1);
+    
+    return (
+        <div className="tab_banner">
+            <div className="tabs_top">
+                <button className={tab == 1 ? "tab_btn active" : "tab_btn"} type="button" onClick={()=>setTab(1)}>Sell</button>
+                <button className={tab == 2 ? "tab_btn active" : "tab_btn"} type="button" onClick={()=>setTab(2)}>Buy</button>
+                <button className={tab == 3 ? "tab_btn active" : "tab_btn"} type="button" onClick={()=>setTab(3)}>Sell & Buy</button>
+            </div>
+            <div className="tab_content">
+                <form className={tab == 1 ? "active" : ""}>
+                    <div className="flex_input">
+                        <div className="field_col">
+                            <label htmlFor="location">Location</label>
+                            <input type="text" name="location" placeholder="Enter city or zip code" className="input"/>
+                        </div>
+                        <div className="field_col">
+                            <label htmlFor="type">Property Type</label>
+                            <select name="type" className="input">
+                            <option>Choose Property Type</option>
+                            <option>Public</option>
+                            <option>Commercial</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="site_btn">
+                            <Image src="/images/search.svg" width={200} height={200} />
+                            <span>Shop Agents</span>
+                        </button>
+                    </div>
+                </form>
+                <form className={tab == 2 ? "active" : ""}>
+                    <div className="flex_input">
+                        <div className="field_col">
+                            <label htmlFor="location">Location</label>
+                            <input type="text" name="location" placeholder="Enter city or zip code" className="input"/>
+                        </div>
+                        <div className="field_col">
+                            <label htmlFor="type">Property Type</label>
+                            <select name="type" className="input">
+                            <option>Choose Property Type</option>
+                            <option>Public</option>
+                            <option>Commercial</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="site_btn">
+                            <Image src="/images/search.svg" width={200} height={200} />
+                            <span>Shop Agents</span>
+                        </button>
+                    </div>
+                </form>
+                <form className={tab == 3 ? "active" : ""}>
+                    <div className="flex_input">
+                        <div className="field_col">
+                            <label htmlFor="location">Location</label>
+                            <input type="text" name="location" placeholder="Enter city or zip code" className="input"/>
+                        </div>
+                        <div className="field_col">
+                            <label htmlFor="type">Property Type</label>
+                            <select name="type" className="input">
+                            <option>Choose Property Type</option>
+                            <option>Public</option>
+                            <option>Commercial</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="site_btn">
+                            <Image src="/images/search.svg" width={200} height={200} />
+                            <span>Shop Agents</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,66 @@
 import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import BannerTabs from "@/components/banner-tabs";
+import RangeSlider from "@/components/range-slider";
 
 export default function Home() {
+  const[tabSave, setTabSave] = useState(1);
   return (
     <>
     <main>
+      <section className="banner">
+        <div className="contain">
+          <div className="flex">
+            <div className="colL">
+              <h1>Transparent Commissions, Trusted Transactions</h1>
+              <p>Discover and shop the commissions of real estate agents and realtors in your area</p>
+              <BannerTabs/>
+            </div>
+            <div className="colR">
+              <div className="image">
+                <Image src="/images/banner.png" width={500} height={700} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="save_sec">
+        <div className="contain">
+            <div className="sec_heading text-center">
+              <h2>How Much Can You Save With a Veerra Agent?</h2>
+            </div>
+            <div className="save_tabs">
+              <div className="btn_blk text-center">
+                <button className={tabSave == 1 ? "site_btn color" : "site_btn"} type="button" onClick={()=>setTabSave(1)}>As a Seller</button>
+                <button className={tabSave == 2 ? "site_btn color" : "site_btn"} type="button" onClick={()=>setTabSave(2)}>As a Buyer</button>
+              </div>
+              <div className="tab_cntnt">
+                <div className="for_buyer">
+                  <div className="flex">
+                    <div className="col">
+                      <h4>Buyer's Agent Commission</h4>
+                      <RangeSlider min={0} max={3} step={1} />
+                    </div>
+                  </div>
+                  <div className="flex_res">
+                    <div className="res_col">
+                      <h5>Sale Price</h5>
+                      <select name="sale_price" className="input">
+                        <option>$234,454</option>
+                        <option>$334,454</option>
+                      </select>
+                    </div>
+                    <div className="res_col">
+                      <h5>You Saved</h5>
+                      <input type="text" className="input" name="saved" value={"$2,454"}/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </section>
     </main>
     </>
 );
